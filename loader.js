4838718c5458e32ade0327967591f917f114e326
Eward Song <eward.song@gmail.com>
2017-05-14 18:30:32 +0800

    fix: fix error require

--- loader.js
+++ loader.js
@@ -2,7 +2,8 @@ module.exports = function(content) {
   this.cacheable();
   return content + `
     if(module.hot) {
-      const cssReload = require('${require.resolve('./hotModuleReplacement')}?${Date.now()}');
+      // ${Date.now()}
+      const cssReload = require('${require.resolve('./hotModuleReplacement')}');
       module.hot.dispose(cssReload);
       module.hot.accept(undefined, cssReload);
     }
