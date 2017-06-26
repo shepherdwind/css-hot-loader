c83b239d8a0833245df96a545b703db3a5c35524
Eward Song <eward.song@gmail.com>
2017-06-26 23:01:23 +0800

    feat: add examples

---
+++ examples/simple/webpack.config.js
@@ -0,0 +1,36 @@
+const webpack = require('webpack'); // webpack itself
+const path = require('path'); // nodejs dependency when dealing with paths
+const ExtractTextWebpackPlugin = require('extract-text-webpack-plugin'); // require webpack plugin
+
+let config = { // config object
+  entry: {
+    output: './src/index.js', // entry file
+  },
+  output: { // output
+    path: path.resolve(__dirname, 'dist'), // ouput path
+    filename: '[name].js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css/,
+        use: ['css-hot-loader'].concat(ExtractTextWebpackPlugin.extract({  // HMR for styles
+          fallback: 'style-loader',
+          use: ['css-loader'],
+        })),
+      },
+    ] // end rules
+  },
+  plugins: [ // webpack plugins
+    new ExtractTextWebpackPlugin('output.css'),
+  ],
+  devServer: {
+    contentBase: path.resolve(__dirname, 'dist'),
+    inline: true,
+    compress: true, // Enable gzip compression for everything served:
+    hot: true // Enable webpack's Hot Module Replacement feature
+  },
+  devtool: 'eval-source-map', // enable devtool for better debugging experience
+}
+
+module.exports = config;
