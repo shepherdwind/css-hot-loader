3ba21b9d9ac97ce3b500c9be190e476a7097abc5
Olivier Louvignes <olivier@mg-crea.com>
2017-08-11 11:29:38 +0200

    feat(examples): add create-react-app with css-modules example

---
+++ examples/create-react-app/src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+
+import styles from './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className={styles.root}>
+        <div className={styles.header}>
+          <img src={logo} className={styles.logo} alt="logo" />
+          <h2>Welcome to React</h2>
+        </div>
+        <p className={styles.intro}>
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
